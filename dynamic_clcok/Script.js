f9bd6dd3a3882217c14b43ef92796265f80f0746
jayakarmwr <jayakarmwr@gmail.com>
2025-03-17 11:14:04 +0530

    a

---
+++ dynamic_clcok/Script.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body"),
+  hourHand = document.querySelector(".hour"),
+  minuteHand = document.querySelector(".minute"),
+  secondHand = document.querySelector(".second"),
+  modeSwitch = document.querySelector(".mode-switch");
+  
+const updateTime = () => {
+  let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hrToDeg = (date.getHours() / 12) * 360;
+  secondHand.style.transform = `rotate(${secToDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+  hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+};
+
+updateTime();
+setInterval(updateTime, 1000);
